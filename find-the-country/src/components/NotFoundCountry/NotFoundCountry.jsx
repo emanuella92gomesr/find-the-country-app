5e084141a179554a17e0140cdaf6c1cea64948c6
Emanuella Gomes <emanuellagomes6@gmail.com>
2023-02-26 09:46:53 -0300

    Inclusion of NotFoundCountry component

---
+++ find-the-country/src/components/NotFoundCountry/NotFoundCountry.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+const NotFoundCountry = () => {
+    return(
+        <Box className='not-found'>
+            <p>No results found.</p>
+        </Box>
+    );
+};
+
+export default NotFoundCountry;
